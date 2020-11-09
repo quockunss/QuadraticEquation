25ebee56508acc4c6e693de19a1b07ae8869c1c9
quockunss <72240212+quockunss@users.noreply.github.com>
2020-11-09 09:24:06 +0700

    add QuadraticEquation

---
+++ src/Ex14_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex14_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width,height);
+
+
+        System.out.println("your rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
+    }
+
+}
